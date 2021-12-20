3f073dae25017bb3e4bded9c8c6247b6a40460a5
alan88888 <45704365+alan88888@users.noreply.github.com>
2021-12-20 17:21:35 +0800

    Create 線段覆蓋.cpp

---
+++ 線段覆蓋.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+using namespace std;
+typedef struct _node{
+    int a;
+    int b;
+}node;
+bool cmp(node p,node q){
+    if(p.a==q.a) return (p.b>q.b);
+    else return (p.a<p.b);
+}
+node d[10000];
+int main()
+{
+    int n,cnt,s,e;
+    while(scanf("%d",&n)!=EOF){
+        cnt=0;
+        for(int i=0;i<n;i++){
+            scanf("%d%d",&(d[i].a),&(d[i].b));
+        }
+        sort(d,d+n,cmp);
+        for(int i=0;i<n;i++){
+            s=d[i].a;
+            e=d[i].b;
+            while(i<n-1&&d[i+1].a<e){
+                if(d[i+1].b<=e) i++;
+                else {
+                    e=d[i+1].b;
+                    i++;
+                }
+            }
+            cnt=cnt+e-s;
+        }
+        printf("%d\n",cnt);
+    }
+    
+    
+    return 0;
+}
